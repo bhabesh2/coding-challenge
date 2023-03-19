1449a1e0fcb66d66e49162ebcd90dff22e3f9995
bhabesh2 <jsarma058@gmail.com>
2023-03-19 11:30:07 +0530

    uniqe items

--- only uniqe items.js
+++ only uniqe items.js
@@ -41,4 +41,4 @@ function removeDuplicates(arr) {
   const uniqueNums = removeDuplicates(nums);
   console.log(uniqueNums);
   // Output: [1, 2, 3, 4, 5, 6, 7]
-  
+  // output: 1,2,3,4,5,6,7 with spread and join operator.
